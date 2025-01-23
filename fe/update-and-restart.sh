d693e1ace2f2f15e895b26bb857827a47600c8da
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2025-01-23 10:06:00 -0300

    Added update-and-restart script

---
+++ fe/update-and-restart.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Store current commit hash
+CURRENT_COMMIT=$(git rev-parse HEAD)
+
+# Pull updates
+git pull origin branch_name
+
+# Build the app
+npm run build
+
+# If build fails, revert and exit
+if [ $? -ne 0 ]; then
+   echo "Build failed. Rolling back to previous commit..."
+   git reset --hard $CURRENT_COMMIT
+   exit 1
+fi
+
+# Restart app with PM2
+pm2 restart airesderio
