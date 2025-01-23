96e59a339944ac076781b1dc62749c419a0f168d
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2025-01-23 09:06:56 -0300

    First content entries and mockup

---
+++ fe/src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Header from '$lib/Header.svelte';
+	// import { i18n } from '$lib/i18n';
+	// import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	let { children } = $props();
+</script>
+
+
+<Header />
+<!-- <ParaglideJS {i18n}> -->
+<main>
+	{@render children()}
+</main>
+<!-- </ParaglideJS> -->
+
+<style>
+	main {
+		padding: 1rem;
+	}
+</style>
