b329a63cc9fde2efbd433c9b499a93de195bcd70
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2025-01-23 09:06:56 -0300

    First content entries and mockup

---
+++ fe/src/lib/Header.svelte
@@ -0,0 +1,28 @@
+<header>
+	<img src="/aires-de-rio.svg" alt="Aires del Río" />
+	<nav>
+		<a href="/">Home</a>
+		<a href="/edificio">Edificio</a>
+		<a href="/ubicacion">Ubicación</a>
+		<a href="/amenidades">Aménidades</a>
+	</nav>
+</header>
+
+<style>
+    header {
+        display: grid;
+        grid-template-columns: max-content 1fr;
+        align-items: first baseline;
+        justify-items: start;
+        background-color: #CCC;
+        padding: 2rem 1rem 0.5rem 1rem;
+    }
+    nav {
+        display: flex;
+        gap: 1rem;
+        justify-content: space-evenly;
+        width: 100%;
+        font-size: 1.2rem;
+        font-weight: 500;
+    }
+</style>
---
+++ fe/src/routes/edificio/+layout.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import Carousel from '$lib/Carousel.svelte';
+	import type { Snippet } from 'svelte';
+
+    let { children } = $props();
+
+	// We define some snippet blocks for each slide
+	// and collect them into an array.
+	let slides: Snippet[] = [slideOne, slideTwo, slideThree];
+</script>
+
+{#snippet slideOne()}
+    <img src="/foto1.png" alt="Aires de Río" />
+{/snippet}
+
+{#snippet slideTwo()}
+	<div style="background: lightcoral; padding: 1rem;">
+		Slide B content
+	</div>
+{/snippet}
+
+{#snippet slideThree()}
+	<div style="background: lightgreen; padding: 1rem;">
+		Slide C content
+	</div>
+{/snippet}
+
+<article>
+    <Carousel {slides} height="400px" />
+	{@render children()}
+</article>
+
+<style>
+    img {
+        width: 100%;
+        height: auto;
+    }
+</style>
