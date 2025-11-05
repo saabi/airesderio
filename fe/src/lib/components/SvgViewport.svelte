fc7209643b6386509af85e2261af4da90b34f3f6
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2025-11-05 00:17:57 -0300

    Starts porting plain SVG files to Svelte components for easier theming

---
+++ fe/src/lib/components/SvgViewport.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	interface Props {
+		width?: string;
+		height?: string;
+		viewBox?: string;
+		class?: string;
+		children?: Snippet;
+	}
+
+	let {
+		width = '4.5em',
+		height = '4.5em',
+		viewBox = '0 0 24 24',
+		class: className = '',
+		children
+	}: Props = $props();
+</script>
+
+<svg
+	width={width}
+	height={height}
+	viewBox={viewBox}
+	class={className}
+	aria-hidden="true"
+	focusable="false"
+>
+	{#if children}
+		{@render children()}
+	{/if}
+</svg>
+
+<style>
+	svg {
+		display: inline-block;
+		vertical-align: middle;
+	}
+</style>
+
