30d10958c55ccb26d7b068727f4b36f06f4a4024
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2025-12-02 13:28:22 -0300

    chore: remove theme.ts utility file
    - File no longer needed after theme logic consolidation
    - All functionality moved to stores/theme.ts
    - No remaining imports from utils/theme

--- fe/src/lib/utils/theme.ts
+++
@@ -1,4 +0,0 @@
-// Re-export Theme type from store for backwards compatibility
-// TODO: Update all imports to use $lib/stores/theme directly
-// This file can be removed once all usages are migrated
-export type { Theme } from '$lib/stores/theme';
